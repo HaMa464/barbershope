fb5342b75dc6020abc9f0664cc3303597767db64
HaMa464 <hamamofaq90@gmail.com>
2026-01-09 15:33:06 +0300

    Set base path for GitHub Pages
    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 👇 REQUIRED for GitHub Pages
+  base: '/barbershope/',
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
